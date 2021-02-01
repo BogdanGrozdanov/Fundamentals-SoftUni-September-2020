9e18d2579b6ec5df62eb49d25dc62ff4d2f8f41b
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 00:46:26 +0000

    Create 10MultiplicationTable.cs

---
+++ 01.Basic Syntax, Conditional Statements and Loops/01.Basic Syntax, Conditional Statements and Loops -Lab/10.MultiplicationTable/10MultiplicationTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} X {i} = {num*i}");
+            }
+        }
+    }
+}
