94a573027ed00b658fef38c596f229722a05726a
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 10:06:53 +0000

    Create 08MagicSum.cs

---
+++ 03.Arrays/02-ArraysExercise/08MagicSum/08MagicSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int magicNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+
+                int num1 = int.Parse(inputNumbers[i]);
+
+                for (int j = i + 1; j < inputNumbers.Length; j++)
+                {
+                    if (j == inputNumbers.Length)
+                    {
+                        break;
+                    }
+
+                    int num2 = int.Parse(inputNumbers[j]);
+
+                    if (magicNum == (num1 + num2))
+                    {
+                        Console.WriteLine($"{num1} {num2}");
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
