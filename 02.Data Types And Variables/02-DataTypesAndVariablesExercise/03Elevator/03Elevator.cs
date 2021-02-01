df9ec68d8872f4a8b25c8729a17cf8505cbc8237
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:31:39 +0000

    Create 03Elevator.cs

---
+++ 02.Data Types And Variables/02-DataTypesAndVariablesExercise/03Elevator/03Elevator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacityPeople = int.Parse(Console.ReadLine());
+
+            double course = Math.Ceiling( numberOfPeople / (double)capacityPeople);
+            Console.WriteLine(course);
+        }
+
+    }
+}
