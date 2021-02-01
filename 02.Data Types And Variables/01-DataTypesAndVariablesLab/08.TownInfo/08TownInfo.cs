9285ef2081a5152cdb8898d3de3423019b6ad155
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:03:04 +0000

    Create 08TownInfo.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/08.TownInfo/08TownInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08TownInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            double square = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {townName} has population of {population} and area {square} square km.");
+        }
+    }
+}
