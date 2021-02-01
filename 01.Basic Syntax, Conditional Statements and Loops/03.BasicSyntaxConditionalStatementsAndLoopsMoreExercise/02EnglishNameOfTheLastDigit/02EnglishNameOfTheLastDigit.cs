a244012eda2697544ba8ac661f8f6865173fcb2d
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 00:47:21 +0000

    Create 02EnglishNameOfTheLastDigit.cs

---
+++ 01.Basic Syntax, Conditional Statements and Loops/03.BasicSyntaxConditionalStatementsAndLoopsMoreExercise/02EnglishNameOfTheLastDigit/02EnglishNameOfTheLastDigit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EnglishNameOfTheLastDigit
+{
+    class EnglishNameOfTheLastDigit
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            string num =String.Empty;
+            char a = '0';
+            string output = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                num += number[i];
+                
+            }
+            a = num[0];
+
+            switch (a)
+            {
+                case '1':
+                    output = "one";
+                    break;
+                case '2':
+                    output = "two";
+                    break;
+                case '3':
+                    output = "three";
+                    break;
+                case '4':
+                    output = "four";
+                    break;
+                case '5':
+                    output = "five";
+                    break;
+                case '6':
+                    output = "six";
+                    break;
+                case '7':
+                    output = "seven";
+                    break;
+                case '8':
+                    output = "eight";
+                    break;
+                case '9':
+                    output = "nine";
+                    break;
+                case '0':
+                    output = "zero";
+                    break;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
