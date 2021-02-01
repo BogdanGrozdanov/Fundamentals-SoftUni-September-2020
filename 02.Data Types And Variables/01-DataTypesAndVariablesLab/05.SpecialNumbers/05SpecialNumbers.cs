65497e323ae3f9a5025cdc68640ddd55f1254ba6
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:02:33 +0000

    Create 05SpecialNumbers.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/05.SpecialNumbers/05SpecialNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                bool isSpecial = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+
+
+            }
+
+        }
+    }
+}
