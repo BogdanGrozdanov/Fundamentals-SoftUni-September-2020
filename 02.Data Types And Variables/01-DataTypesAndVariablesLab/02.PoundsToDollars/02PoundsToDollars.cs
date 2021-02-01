2e9a6311f24733503f64b2d851fbe22bc258f4ed
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:02:25 +0000

    Create 02PoundsToDollars.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/02.PoundsToDollars/02PoundsToDollars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal output = input * 1.31m;
+            Console.WriteLine($"{output:f3}");
+        }
+    }
+}
