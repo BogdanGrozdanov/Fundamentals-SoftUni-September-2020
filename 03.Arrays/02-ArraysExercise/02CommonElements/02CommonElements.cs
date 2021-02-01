2720c7e5c40a77e005c8f80e36cf51e523b65c29
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 10:06:49 +0000

    Create 02CommonElements.cs

---
+++ 03.Arrays/02-ArraysExercise/02CommonElements/02CommonElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _02CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine().Split(" ").ToArray();
+            string[] secoundArr = Console.ReadLine().Split(" ").ToArray();
+
+            foreach (string output in secoundArr)
+            {
+                for (int i = 0; i < firstArr.Length; i++)
+                {
+                    if (output==firstArr[i])
+                    {
+                        Console.Write(output + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
