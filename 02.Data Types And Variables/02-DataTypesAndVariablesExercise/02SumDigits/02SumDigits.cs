0bf2497a5edd440d1a92f3e42cc8d71b3d1fffa3
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:31:35 +0000

    Create 02SumDigits.cs

---
+++ 02.Data Types And Variables/02-DataTypesAndVariablesExercise/02SumDigits/02SumDigits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currentNumber = (int)Char.GetNumericValue(input[i]);
+                sum += currentNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
