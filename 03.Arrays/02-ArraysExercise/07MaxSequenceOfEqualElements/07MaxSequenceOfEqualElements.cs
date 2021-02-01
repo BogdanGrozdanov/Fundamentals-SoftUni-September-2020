627b158a12481960de1d12e309a25acc649700ec
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 10:06:55 +0000

    Create 07MaxSequenceOfEqualElements.cs

---
+++ 03.Arrays/02-ArraysExercise/07MaxSequenceOfEqualElements/07MaxSequenceOfEqualElements.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _07MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                               .Split(" ")
+                               .ToArray();
+            int counter = 0;
+            int bestIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string currentElement = array[i];
+                int currentCounter = 1;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (currentElement == array[j])
+                    {
+                        currentCounter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (currentCounter > counter)
+                {
+                    counter = currentCounter;
+                    bestIndex = i;
+                }
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                Console.Write(array[bestIndex] + " ");
+            }
+            
+
+        }
+    }
+}
