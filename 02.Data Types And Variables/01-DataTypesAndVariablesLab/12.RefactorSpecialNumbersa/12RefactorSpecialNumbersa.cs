5b2edf166463fd9b98de2a36d4dc9e1e39ec5d96
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:02:39 +0000

    Create 12RefactorSpecialNumbersa.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/12.RefactorSpecialNumbersa/12RefactorSpecialNumbersa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _12RefactorSpecialNumbersa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumDigits = 0;
+            int takova = 0;
+            bool isSpecial = false;
+            for (int num = 1; num <= n; num++)
+            {
+                takova = num;
+                while (num > 0)
+                {
+                    sumDigits += num % 10;
+                    num = num / 10;
+                }
+                isSpecial = (sumDigits == 5) || (sumDigits == 7) || (sumDigits == 11);
+                sumDigits = 0;
+                num = takova;
+                Console.WriteLine("{0} -> {1}", num, isSpecial);
+
+            }
+
+        }
+    }
+}
