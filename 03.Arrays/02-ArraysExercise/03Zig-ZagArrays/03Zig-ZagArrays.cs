79e9ba09dd1251da0e91ce17d535a928993eeb44
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 10:06:42 +0000

    Create 03Zig-ZagArrays.cs

---
+++ 03.Arrays/02-ArraysExercise/03Zig-ZagArrays/03Zig-ZagArrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] inputArr = new int[2];
+            string[] arr1 = new string[n];
+            string[] arr2 = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentArray = Console.ReadLine().Split(" ").ToArray();
+                
+                if (i % 2 == 0)
+                {
+                    arr1[i] = currentArray[0];
+                    arr2[i] = currentArray[1];
+                }
+                else if (i % 2 == 1)
+                {
+                    arr1[i] = currentArray[1];
+                    arr2[i] = currentArray[0];
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", arr1));
+            Console.WriteLine(string.Join(" ", arr2));
+        }
+    }
+}
