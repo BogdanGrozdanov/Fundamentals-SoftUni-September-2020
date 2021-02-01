a1cb8736490d9a3d76c5525eb9634cb467e53616
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:02:35 +0000

    Create 10LowerOrUpper.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/10.LowerOrUpper/10LowerOrUpper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            int lower = (int)input;
+
+            if (lower>=65&&lower<=90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (lower>=97&&lower<=122)
+            {
+                Console.WriteLine("lower-case");
+            }
+
+            
+
+            
+        }
+    }
+}
