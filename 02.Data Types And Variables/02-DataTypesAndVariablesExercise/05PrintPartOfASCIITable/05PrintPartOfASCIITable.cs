537756a9feb400c863e1aec9a4c3e46b3cea3190
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:31:41 +0000

    Create 05PrintPartOfASCIITable.cs

---
+++ 02.Data Types And Variables/02-DataTypesAndVariablesExercise/05PrintPartOfASCIITable/05PrintPartOfASCIITable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace _05PrintPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                char output = (char)i;
+                
+                Console.Write($"{output} ");
+            }
+        }
+    }
+}
