68e3f51eef808cf3caca15a6aed8f8e32d8bc0f4
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 10:06:46 +0000

    Create 04ArrayRotation.cs

---
+++ 03.Arrays/02-ArraysExercise/04ArrayRotation/04ArrayRotation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArr = Console.ReadLine().Split(" ");
+            int rotationNum = int.Parse(Console.ReadLine());
+           
+
+            for (int i = 0; i < rotationNum; i++)
+            {
+                string rotationElement = inputArr[0];
+
+                for (int j = 1; j <inputArr.Length; j++)
+                {
+                    string currentElement = inputArr[j];
+                    inputArr[j - 1] = currentElement;
+                }
+                inputArr[inputArr.Length - 1] = rotationElement;
+            }
+            Console.WriteLine(string.Join(" ",inputArr));
+        }
+    }
+}
