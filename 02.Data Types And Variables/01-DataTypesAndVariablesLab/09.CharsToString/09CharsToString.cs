23df6e528c29d29965c541e0669a3d5ad20f99c8
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:02:41 +0000

    Create 09CharsToString.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/09.CharsToString/09CharsToString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirtChar = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{firstChar}{secondChar}{thirtChar}");
+        }
+    }
+}
