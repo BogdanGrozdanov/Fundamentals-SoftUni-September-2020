b0b6aa6154e8adbe5f19e0453dc3e4390a05c796
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 10:06:44 +0000

    Create 05TopIntegers.cs

---
+++ 03.Arrays/02-ArraysExercise/05TopIntegers/05TopIntegers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _05TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse)
+                       .ToArray();
+            bool isBiger = true;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentInt = arr[i];
+
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j]>=currentInt)
+                    {
+                        isBiger = false;
+                        break;
+                    }
+                }
+                if (isBiger)
+                {
+                    Console.Write(currentInt+ " ");
+                }
+                isBiger = true;
+            }
+        }
+    }
+}
