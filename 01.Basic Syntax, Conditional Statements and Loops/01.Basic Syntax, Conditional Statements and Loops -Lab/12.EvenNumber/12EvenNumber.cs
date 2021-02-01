47417d27c626fca43c70a9032e5ab734ed7dc9b6
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 00:46:16 +0000

    Create 12EvenNumber.cs

---
+++ 01.Basic Syntax, Conditional Statements and Loops/01.Basic Syntax, Conditional Statements and Loops -Lab/12.EvenNumber/12EvenNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace EvenNumber
+{
+    class EvenNumber
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            while (num % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {Math.Abs(num)}");
+        }
+    }
+}
