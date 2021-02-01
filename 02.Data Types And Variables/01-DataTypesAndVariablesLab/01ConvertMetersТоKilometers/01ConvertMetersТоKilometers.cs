185e262ce866e3f403ae863d1fec5d777466e07e
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 00:55:55 +0000

    Create 01ConvertMetersТоKilometers.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/01ConvertMetersТоKilometers/01ConvertMetersТоKilometers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01ConvertMetersТоKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            float output = input * 0.001f;
+            Console.WriteLine($"{output:f2}");
+        }
+    }
+}
