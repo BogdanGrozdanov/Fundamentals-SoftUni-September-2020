df2ce986a5883ef609a4db24d1239c5fa4950351
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 00:46:38 +0000

    Create 02Division.cs

---
+++ 01.Basic Syntax, Conditional Statements and Loops/02.Basic Syntax Conditional Statements And Loops-Exercise/02Division/02Division.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Division
+{
+    class Division
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int division = 0;
+
+            if (input % 10 == 0)
+            {
+                division = 10;
+            }
+            else if (input % 7 == 0)
+            {
+                division = 7;
+            }
+            else if (input % 6 == 0)
+            {
+                division = 6;
+            }
+            else if (input % 3 == 0)
+            {
+                division = 3;
+            }
+            else if (input % 2 == 0)
+            {
+                division = 2;
+            }
+            else
+            {
+                division = 0;
+            }
+
+            if (division!=0)
+            {
+                Console.WriteLine($"The number is divisible by {division}");
+            }
+            else
+            {
+                Console.WriteLine($"Not divisible");
+            }
+            
+        }
+    }
+}
