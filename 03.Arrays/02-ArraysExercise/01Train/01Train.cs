090aeb29258d6466657f61d3d1afb71f19ec2b1e
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 10:06:40 +0000

    Create 01Train.cs

---
+++ 03.Arrays/02-ArraysExercise/01Train/01Train.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace _01Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagon = int.Parse(Console.ReadLine());
+            int allPeople = 0;
+            int[] people = new int[wagon];
+            for (int i = 0; i < wagon; i++)
+            {
+                people[i] = int.Parse(Console.ReadLine());
+                allPeople += people[i];
+            }
+            foreach (var peopleInWagon in people)
+            {
+                Console.Write(peopleInWagon + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(allPeople);
+        }
+    }
+}
