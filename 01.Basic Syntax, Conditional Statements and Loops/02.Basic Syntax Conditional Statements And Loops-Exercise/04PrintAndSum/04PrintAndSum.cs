e56f399013a152f189163addb7ff448b0640240c
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 00:46:19 +0000

    Create 04PrintAndSum.cs

---
+++ 01.Basic Syntax, Conditional Statements and Loops/02.Basic Syntax Conditional Statements And Loops-Exercise/04PrintAndSum/04PrintAndSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    class PrintAndSum
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                Console.Write($"{startNum} ");
+                sum += startNum;
+                startNum++;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
