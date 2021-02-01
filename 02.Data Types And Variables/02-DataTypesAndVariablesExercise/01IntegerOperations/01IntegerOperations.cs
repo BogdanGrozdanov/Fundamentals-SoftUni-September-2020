e3a45c5edc2daab6b26823066fb1642c9f73b46f
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:31:33 +0000

    Create 01IntegerOperations.cs

---
+++ 02.Data Types And Variables/02-DataTypesAndVariablesExercise/01IntegerOperations/01IntegerOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourttNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(((firstNumber+secondNumber)/thirdNumber)*fourttNumber);
+        }
+    }
+}
