cb301ce09cd608385880cc08547f1ac7e2e8df2b
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:02:44 +0000

    Create 06ReversedChars.cs

---
+++ 02.Data Types And Variables/01-DataTypesAndVariablesLab/06.ReversedChars/06ReversedChars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _06ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar= char.Parse(Console.ReadLine());
+            char thirtChar= char.Parse(Console.ReadLine());
+            Console.WriteLine($"{thirtChar} {secondChar} {firstChar}");
+        }
+    }
+}
