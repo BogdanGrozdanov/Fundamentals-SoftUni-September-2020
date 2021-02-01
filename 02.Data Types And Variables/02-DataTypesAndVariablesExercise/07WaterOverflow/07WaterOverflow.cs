8af2f013b8dbf5497731a554bd91e5d693751ac1
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:31:43 +0000

    Create 07WaterOverflow.cs

---
+++ 02.Data Types And Variables/02-DataTypesAndVariablesExercise/07WaterOverflow/07WaterOverflow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int capacity = 255;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int addCapacity = int.Parse(Console.ReadLine());
+                sum += addCapacity;
+
+                if (sum > capacity)
+                {
+                    sum -= addCapacity;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
