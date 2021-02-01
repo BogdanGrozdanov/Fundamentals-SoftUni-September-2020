2903865b666f3b06d463772b93194b82540ff93a
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-02-01 09:31:47 +0000

    Create 08BeerKegs.cs

---
+++ 02.Data Types And Variables/02-DataTypesAndVariablesExercise/08BeerKegs/08BeerKegs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _08BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double biggerKeg = 0;
+            string output = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string modelKeg = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volumeKeg = Math.PI * Math.Pow(radius, 2) * height;
+                if (biggerKeg < volumeKeg)
+                {
+                    biggerKeg = volumeKeg;
+                    output = modelKeg;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
